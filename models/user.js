5a06ec8f671ba78829d303c8749e9120106a4935
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-10-04 20:46:27 -0500

    gettingstarted.hbs added.

--- models/user.js
+++ models/user.js
@@ -50,11 +50,11 @@ module.exports = (sequelize, Sequelize) => {
 		underscored: true
 	});
 	
-	User.associate = (models) => {
-		User.hasMany(models.Investment, {
-			onDelete: 'cascade'
-		});
-	};
+	// User.associate = (models) => {
+	// 	User.hasMany(models.Investment, {
+	// 		onDelete: 'cascade'
+	// 	});
+	// };
 	
 	return User;
 }
